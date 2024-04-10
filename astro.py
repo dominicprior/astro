458438515c29238d894c35d94949d78cb9a8ecc2
Dominic Prior <dominicprior42@gmail.com>
2024-04-10 18:48:01 +0100

    Update astro.py

--- astro.py
+++ astro.py
@@ -1,4 +1,8 @@
-# Calculate the XYZ dir given the alt, az, lat and long
+# Calculate the XYZ dir given the alt, az, lat and long.
+# The axes are:
+# X going out through the equator at longitude 0
+# Y going out through the equator at longitude 90 East
+# Z going out through the North pole
 
 import math
 import sys
@@ -12,16 +16,19 @@ def rot(x, y, angle):
     s = math.sin(a)
     return c * x - s * y, s * x + c * y
 
-# Returns the result of rotating the vector, v, about the X axis
-# clockwise through the given angle (in degrees).
+# These 3 functions return a vector that is a rotation of v about an axis by
+# the angle in degrees.
+# The rotation is clockwise when looking along the axis.
 
 def rotAboutX(v, angle):
     [x, y, z] = v
     y_, z_ = rot(y, z, angle)
     return [x, y_, z_]
 
-# Returns the result of rotating the vector, (x, y, z), about the X axis
-# clockwise through the given angle (in degrees).
+def rotAboutY(v, angle):
+    [x, y, z] = v
+    z_, x_ = rot(z, x, angle)
+    return [x_, y, z_]
 
 def rotAboutZ(v, angle):
     [x, y, z] = v
@@ -31,17 +38,28 @@ def rotAboutZ(v, angle):
 def prVec(v):
     print(f'{v[0]:,.4f}, {v[1]:,.4f}, {v[2]:,.4f}')
 
+if (len(sys.argv) != 5):
+    print('\nExpects 4 args: alt, az, lat, long.')
+    print('    (The args are in alphabetical order).')
+    exit(1)
+
 alt  = float(sys.argv[1])
 az   = float(sys.argv[2])
 lat  = float(sys.argv[3])
 long = float(sys.argv[4])
 
-v0 = [0, 1, 0]
-v1 = rotAboutX(v0, alt)
-prVec(v1)
-v2 = rotAboutZ(v1, -az)
-prVec(v2)
-v3 = rotAboutX(v2, 90 - lat)
-prVec(v3)
-v4 = rotAboutZ(v3, 90 + long)
+v0 = [0, 0, 1]
+v1 = rotAboutY(v0, alt)
+# prVec(v1)
+v2 = rotAboutX(v1, -az)
+# prVec(v2)
+
+# v2 is the XYZ for alt and az for someone standing at 0° latitude
+# and 0° longitude.
+
+# Then we move the person to the correct latitude and longitude.
+
+v3 = rotAboutY(v2, -lat)
+# prVec(v3)
+v4 = rotAboutZ(v3, long)
 prVec(v4)
